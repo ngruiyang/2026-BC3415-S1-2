3d3401781f5caf2cd0a473f2cc66f53a346ec484
ngruiyang <73708403+ngruiyang@users.noreply.github.com>
2026-08-11 08:10:13 +0000

    67

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+#DBS Prediction
 
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+#GET from FrontEnd, POST to FrontEnd
+@app.route("/",methods=["GET","POST"])
+def index():
+    return("hi")
+
+if __name__ == "__main__":
+    app.run()
